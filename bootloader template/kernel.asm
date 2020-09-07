b8cd12c9aa8990d599d813cf2a3875be15f78301
koga25 <66087274+koga25@users.noreply.github.com>
2020-09-07 00:36:06 -0300

    Update kernel.asm

--- bootloader template/kernel.asm
+++ bootloader template/kernel.asm
@@ -4,6 +4,9 @@ jmp 0x0000:start
 data:
 	playerPosition: dw 0
     tablePosition: dw 0
+    enemyPosition: dw 0
+    endGame: db "Game end", 0
+    len equ endGame - $
 	;Dados do projeto...
 
 start:
@@ -13,8 +16,9 @@ start:
     mov cl, 1
     mov bl, 0
     mov bh, 0
-    mov word [playerPosition], 1h
+    mov word [playerPosition], 0x2000
     mov word [tablePosition], 1
+    mov word [enemyPosition], 0
     call makegrid
     ;mov al, 51h
     ;call putchar
@@ -41,22 +45,26 @@ makegrid:
             ;call putchar
             ;call jumpLine
             call clear
+            call enemyMovement
             jmp .gridBegin
         .sPressed:
             ;call putchar
             ;call jumpLine
             call clear
+            call enemyMovement
             jmp .gridBegin
         .aPressed:
             ;call putchar
             ;call jumpLine
             call clear
+            call enemyMovement
             shl word [playerPosition], 1
             jmp .gridBegin
         .dPressed:
             ;call putchar
             ;call jumpLine
             call clear
+            call enemyMovement
             shr word [playerPosition], 1
             jmp .gridBegin
     .gridBegin:
@@ -85,22 +93,63 @@ clear:
     int 10h
     ret
 
+enemyMovement:
+    cmp word[enemyPosition], 0 
+    je notSpawned
+    jne spawned
+    notSpawned:
+        mov word[enemyPosition], 2
+        ret
+    spawned:
+        shl word[enemyPosition], 3
+        ret
+
+collision:
+
+    mov ax, [word playerPosition]
+    cmp ax, word[enemyPosition]
+    je collided
+    jne notCollided
+    notCollided:
+        ret
+    collided:
+        call clear
+        mov si, endGame
+        call end1
+        jmp $
+
 
 putchar:
     ;this puts the char in the screen.
     mov ax, word[playerPosition]
     cmp ax, word[tablePosition]
-    mov ah, 0eh
     je playerEncountered
     jne playerNotEncountered
+
     playerNotEncountered:
+        mov ax, word[enemyPosition]
+        cmp ax, word[tablePosition]
+        mov ah, 0eh
+        je enemyEncountered
+        jne enemyNotEncountered
+
+        enemyEncountered:
+        mov al, 118
+        int 10h
+        ret
+
+        enemyNotEncountered:
         mov al, 46
         int 10h
         ret
+
     playerEncountered:
+        call collision
+        mov ah, 0eh
         mov al, 64
         int 10h
         ret
+
 jumpLine:
     mov ah, 0eh
     mov al, 10
@@ -109,5 +158,14 @@ jumpLine:
     int 10h
     ret
 
+end1:
+    lodsb
+    mov ah, 0xe
+    mov bh, 0
+    mov bl, 0xf
+    int 10h
 
-jmp $
+    cmp al, 0
+    jne end1
+    ret
+
